af4c190664bfb809e9261eb76c2a06d2aa433a73
Adrien P <ap@cdfr.net>
2017-12-06 16:00:14 +0000

    Initial commit

---
+++ Source/Configuration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;  //we need this for keycode object
+
+namespace forceRes
+{
+    public class Configuration
+    {
+        // 0\1 means you're logging some basic information, basic startup data perhaps.
+        // 2 = Detailed loggin used to for person to troubleshoot, or info they might need to send you the developer.
+        // 3+ development\extreme logging only meant for you during development to check someting.
+        public bool DebugLogging = false;
+        public byte DebugLoggingLevel = 0;
+
+        public bool UseCustomLogFile = false;  //our setting to the custom log file name or not.
+        public string CustomLogFilePath = "forceResName_Log.txt";  //name of our default customlog filename
+
+        public int gameResolutionVertical = 1600;
+        public int gameResolutionHorizontal = 3840;
+        public bool gameFullscreen = true;
+
+        public Configuration() { }
+
+
+        //Save the given instance of your config data to disk as XML
+        public static void Serialize(string filename, Configuration config)
+        {
+            var serializer = new XmlSerializer(typeof(Configuration));
+            try
+            {
+                using (var writer = new StreamWriter(filename))
+                {
+                    serializer.Serialize(writer, config);
+                }
+            }
+            catch (System.IO.IOException ex1)
+            {
+                Logger.dbgLog("Filesystem or IO Error: \r\n", ex1, true);
+            }
+            catch (Exception ex1)
+            {
+                Logger.dbgLog(ex1.Message.ToString() + "\r\n", ex1, true);
+            }
+        }
+
+        //Load your config data to disk as XML into an instance of this object and return it.
+        public static Configuration Deserialize(string filename)
+        {
+            var serializer = new XmlSerializer(typeof(Configuration));
+
+            try
+            {
+                using (var reader = new StreamReader(filename))
+                {
+                    var config = (Configuration)serializer.Deserialize(reader);
+                    ValidateConfig(ref config);
+                    return config;
+                }
+            }
+            
+            catch(System.IO.FileNotFoundException ex4)
+            {
+                Logger.dbgLog("config file not found. This is expected if no config file. \r\n", ex4, false);
+            }
+
+            catch (System.IO.IOException ex1)
+            {
+                Logger.dbgLog("Filesystem or IO Error: \r\n", ex1, true);
+            }
+            catch (Exception ex1)
+            {
+                Logger.dbgLog(ex1.Message.ToString() + "\r\n", ex1, true);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Constrain certain values read in from the config file that will either cause issue or just make no sense. 
+        /// </summary>
+        /// <param name="tmpConfig"> An instance of an initialized Configuration object (*byref*)</param>
+        public static void ValidateConfig(ref Configuration tmpConfig)
+        {
+            //pass
+            //if (tmpConfig.gameResolutionVertical > 6000 | tmpConfig.gameResolutionVertical < 600 ) tmpConfig.gameResolutionVertical = 768;
+            //if (tmpConfig.gameResolutionHorizontal > 6000 | tmpConfig.gameResolutionHorizontal < 800) tmpConfig.gameResolutionHorizontal = 1024;
+        }
+    }
+}
---
+++ Source/ForceRes.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+using ColossalFramework.UI;
+using ColossalFramework;
+using UnityEngine;
+
+namespace forceRes
+{
+    // The class will inherit (take on the base properties of and implment..) the IUserMod interface found in ICities. 
+    public class forceResName : IUserMod
+    {
+        internal const string MOD_NAME = "ForceRes";
+        internal const string MOD_DESC = "Force the screen resolution";
+        internal const string MOD_LOG_PREFIX = "ForceRes";
+        internal const string MOD_CONFIGPATH = "ForceRes_Config.xml";
+        internal static bool DEBUG_LOG_ON = false;
+        internal static byte DEBUG_LOG_LEVEL = 0;
+
+        internal static bool isEnabled = false;
+        internal static bool isInited = false;
+        internal static Configuration config;
+
+
+        public string Name
+        {
+            get
+            {
+                return MOD_NAME;
+            }
+ 
+        } 
+
+
+        public string Description
+        {
+            get
+            {
+                return MOD_DESC;
+            }
+        }
+
+
+        public void forceRes()
+        {
+            Logger.dbgLog(forceResName.MOD_NAME + " has been loaded.");
+
+        }
+
+
+        public void OnEnabled()
+        {
+            isEnabled = true;
+            Logger.dbgLog(forceResName.MOD_NAME + " has been enabled.");
+            Do_Init();
+        }
+
+
+        public void OnDisabled()
+        {
+            isEnabled = false;
+        }
+
+
+        private void Do_Init()
+        {
+            Helper.ReloadConfigValues(false, false);
+        }
+
+
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            try
+            {
+                UIHelperBase group = helper.AddGroup("ForceRes");
+                group.AddTextfield("Horizontal ", "3840", OnChangeHorizontalResolution);
+                group.AddTextfield("Vertical", "1600", OnChangeVerticalResolution);
+                group.AddCheckbox("Fullscreen", config.gameFullscreen, OnChangeFullscreen);
+
+                group.AddCheckbox("Enable Verbose Logging", DEBUG_LOG_ON, AutoShowChecked);
+
+                group.AddSpace(16);
+                group.AddButton("Save", SaveConfigFile);
+            }
+            catch (Exception ex)
+            {
+                Logger.dbgLog("Error in settings panel.", ex, true);
+            }
+        }
+
+
+        private void OnChangeFullscreen(bool bValue)
+        {
+            config.gameFullscreen = bValue;
+            Configuration.Serialize(MOD_CONFIGPATH, config);
+        }
+
+
+        private void OnChangeHorizontalResolution(string sValue)
+        {
+            try {
+                config.gameResolutionHorizontal = Int32.Parse(sValue);
+            }
+            catch (Exception ex)
+            {
+                Logger.dbgLog("Error: invalid value ?", ex, true);
+            }
+        }
+
+        private void OnChangeVerticalResolution(string sValue)
+        {
+            try {
+                config.gameResolutionVertical = Int32.Parse(sValue);
+            }
+            catch (Exception ex)
+            {
+                Logger.dbgLog("Error: invalid value ?", ex, true);
+            }
+        }
+
+
+        private void AutoShowChecked(bool bValue)
+        {
+            config.AutoShowOnMapLoad = bValue; 
+            Configuration.Serialize(MOD_CONFIGPATH, config); 
+        }
+
+
+        private void SaveConfigFile()
+        {
+            Screen.SetResolution(config.gameResolutionHorizontal, config.gameResolutionVertical, config.gameFullscreen);
+
+            Configuration.Serialize(MOD_CONFIGPATH,config);
+        }
+
+    }
+}
